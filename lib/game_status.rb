c217762eabcd9d9e4d60de32ff435440ccae3284
Michael <wtfdic@hotmail.com>
2020-11-12 16:59:01 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,7 +27,7 @@ def won?(board)
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combo
-    if position_1 == "O" && position_2 == "O" && position_3 == "O"
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combo
     end
   }
